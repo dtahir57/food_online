e1bc178a8a0cee98b862025cca9d2f96cd7ed11e
Macbook Pro <macbookpro@Macbooks-MacBook-Pro-2.local>
2024-05-19 21:25:46 +0500

    Registering user

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from . forms import UserForm
 from . models import User
+from django.contrib import messages
 
 def registerUser(request):
     if request.method == 'POST':
@@ -24,7 +25,7 @@ def registerUser(request):
             user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
             user.role = User.CUSTOMER
             user.save()
-
+            messages.success(request, 'Your account has been registered successfully!')
             return redirect('registerUser')
         else:
             print(form.errors)
